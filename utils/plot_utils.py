3357d7527552f61a90f3b7275b6d24c30bcfeb86
Zulkhuu Tuya <t.zulkhuu@gmail.com>
2020-12-18 23:35:11 +0900

    add: plot utils

---
+++ utils/plot_utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plt_imlist(images, fig_size=(20,10)):
+    plt.figure()
+    plt.rcParams["figure.figsize"] = fig_size
+    fig, axs = plt.subplots(1, len(images))
+    for i in range(len(images)):
+        if len(images[i].shape) == 2:
+            # Binary image
+            axs[i].imshow(np.dstack((images[i], images[i], images[i]))*255)
+        else:
+            axs[i].imshow(images[i])
