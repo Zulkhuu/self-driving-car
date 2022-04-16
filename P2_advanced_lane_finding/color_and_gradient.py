d8930a91e0501a2784d5df9f41b73dfcd7b0fd8f
Zulkhuu Tuya <t.zulkhuu@gmail.com>
2022-04-16 21:56:22 +0900

    Add interactive tuning for color processing

--- P2_advanced_lane_finding/color_and_gradient.py
+++ P2_advanced_lane_finding/color_and_gradient.py
@@ -1,39 +1,27 @@
 import numpy as np
 import cv2
 
-def hls_select(img, thresh=(0, 255)):
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    # Convert to HLS color space and separate the S channel
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     s_channel = hls[:,:,2]
-    binary_output = np.zeros_like(s_channel)
-    binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
-    return binary_output
-
-def pipeline(img, s_thresh=(110, 255), sx_thresh=(20, 100)):
-    img = np.copy(img)
-    # Convert to HLS color space and separate the V channel
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    l_channel = hls[:,:,1]
-    s_channel = hls[:,:,2]
-    # Sobel x
-    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    
+    # Apply Sobel filter in horizontal direction
+    sobelx = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
     scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
 
-    # Threshold x gradient
+    # Threshold x gradient magnitude
     sxbinary = np.zeros_like(scaled_sobel)
     sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
 
-    # Threshold color channel
+    # Threshold Saturation channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
-    # Stack each channel
-    #color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+    
+    # Combine S channel and gradient thresholds
     ret = np.zeros_like(s_channel)
     ret[(s_binary == 1) | (sxbinary == 1)] = 1
-
-    # Add hue filtering
-    h_filtered = cv2.inRange(hls, (0, 0, 0), (40, 255, 255))
-    ret2 = np.zeros_like(s_channel)
-    ret2 = s_binary | ((h_filtered//255) & sxbinary)
-
-    return ret2
+    
+    return ret
